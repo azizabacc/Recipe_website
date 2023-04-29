5ade89177180e0c99d21531c2293723323144fa8
Aziza Baccouri <aizabaccouri@gmail.com>
2023-04-29 12:16:56 +0200

    "V1.1"

--- display_recipe.php
+++ display_recipe.php
@@ -1,89 +1,3 @@
-<?php
-
-$users = [
-    [
-        'full_name' => 'Mickaël Andrieu',
-        'email' => 'mickael.andrieu@exemple.com',
-        'age' => 34,
-    ],
-    [
-        'full_name' => 'Mathieu Nebra',
-        'email' => 'mathieu.nebra@exemple.com',
-        'age' => 34,
-    ],
-    [
-        'full_name' => 'Laurène Castor',
-        'email' => 'laurene.castor@exemple.com',
-        'age' => 28,
-    ],
-];
-
-$recipes = [
-    [
-        'title' => 'Cassoulet',
-        'recipe' => '',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Couscous',
-        'recipe' => '',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => false,
-    ],
-    [
-        'title' => 'Escalope milanaise',
-        'recipe' => '',
-        'author' => 'mathieu.nebra@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Salade Romaine',
-        'recipe' => '',
-        'author' => 'laurene.castor@exemple.com',
-        'is_enabled' => false,
-    ],
-];
-//new function to display enabled profils
-function display_recipe(array $recipe) : string
-{
-    $recipe_content = '';
-
-    if ($recipe['is_enabled']) {
-        $recipe_content = '<article>';
-        $recipe_content .= '<h3>' . $recipe['title'] . '</h3>';
-        $recipe_content .= '<div>' . $recipe['recipe'] . '</div>';
-        $recipe_content .= '<i>' . $recipe['author'] . '</i>';
-        $recipe_content .= '</article>';
-    }
-    
-    return $recipe;
-}
-
-function display_author(string $authorEmail, array $users) : string
-{
-    for ($i = 0; $i < count($users); $i++) {
-        $author = $users[$i];
-        if ($authorEmail === $author['email']) {
-            return $author['full_name'] . '(' . $author['age'] . ' ans)';
-        }
-    }
-}
-
-function get_recipes(array $recipes) : array
-{
-    $valid_recipes = [];
-
-    foreach($recipes as $recipe) {
-        if ($recipe['is_enabled']) {
-            $valid_recipes[] = $recipe;
-        }
-    }
-
-    return $valid_recipes;
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -97,8 +11,13 @@ function get_recipes(array $recipes) : array
     <!-- header -->
     <?php include_once("includes/header.php");?>
     <!-- header -->
+    <!-- include variables and functions -->
+    <?php
+    include_once('includes/variables.php');
+    include_once('includes/functions.php');
+    ?>
     <div class="container">
-        <h1>Liste des recettes</h1>
+        <h1>Recipes List</h1>
         <!-- Plus facile à lire -->
         <?php foreach(get_recipes($recipes) as $recipe) : ?>
             <article>
--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,7 @@
-<!-- footer -->
-<p>Copyright moi, tous droits réservés</p>
+<!-- footer.php -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2021 Copyright:
+    <a class="text-dark" href="https://openclassrooms.com/">OpenClassrooms</a>
+  </div>
+</footer>
